c6763283745dfb7432238b4d1fd55ec255da7d1f
Lucemans <luc@lucemans.nl>
2022-06-26 22:01:18 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<a href="https://luc.community" target="_blank">
+    <img src="./public/banner.png" style="width: 100%" />
+</a>
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,ts,tsx,css}'],
+    theme: {
+        extend: {
+            colors: {
+                discord: {
+                    1: 'rgb(88, 101, 242)',
+                    2: 'rgb(71, 82, 196)',
+                },
+                twitter: {
+                    1: 'rgb(29, 155, 240)',
+                    2: 'rgb(26, 140, 216)',
+                },
+                lens: {
+                    1: 'rgba(171,254,44,.8)',
+                    2: 'rgb(0, 80, 30)',
+                },
+                instagram: {
+                    1: '#f30005',
+                    2: '#ba00af',
+                },
+                neutral: {
+                    900: '#121212',
+                },
+            },
+        },
+    },
+    plugins: [],
+};
